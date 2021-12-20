a3e91b6caf0581895c1b7975b41a5647d60390b0
rakshian24 <rakshithrao123@gmail.com>
2021-12-20 14:09:48 +0530

    Init setup

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import AppNavContainer from './src/navigations';
+
+const App = () => {
+  return <AppNavContainer />;
+};
+
+export default App;
---
+++ src/navigations/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView, Text} from 'react-native';
+
+const AppNavContainer = () => {
+  return (
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>Hey Raksh</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavContainer;
